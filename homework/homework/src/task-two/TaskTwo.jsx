4602ab7d768f9c3c34c5be7f6cc4bef287bfb0ea
mongg666 <nerokss555@gmail.com>
2025-07-04 14:50:24 +0700

    Update TaskTwo.jsx

--- homework/homework/src/task-two/TaskTwo.jsx
+++ homework/homework/src/task-two/TaskTwo.jsx
@@ -1,42 +1,67 @@
-import React from 'react'
+import React, { useCallback, useMemo, memo, useRef } from 'react';
 import RenderCounter from './render-counter/RenderCounter';
 import './TaskTwo.css';
 
-export default function TaskTwo() {
-    const update = useUpdate()
-    return (
-        <div className="TaskTwo">
-            <button onClick={update}>Обновить компонент</button>
-            {/*<RenderCounter />*/}
-            <Root />
-        </div>
-    )
-}
-
-const Root = () => {
-    const [value, setValue] = React.useState('')
-    const handleChange = (event) => {
-        setValue(event.target.value)
-    }
-    return (
-        <form className="form-container">
-            Введенное значение: {value}
-            {/*<RenderCounter />*/}
-            <Input onChange={handleChange} />
-        </form>
-    )
-}
-
-const Input = ({ onChange }) => {
-    return (
-        <div className="input-container">
-            <input type="text" className="input-field" name="value" onChange={onChange} />
-            {/*<RenderCounter />*/}
-        </div>
-    )
-}
-
+// Хук для принудительного обновления компонента
 function useUpdate() {
-    const [, setCount] = React.useState(0)
-    return () => { setCount(counter => counter + 1) }
+  const [, setCount] = React.useState(0);
+  return useCallback(() => {
+    setCount(counter => counter + 1);
+  }, []);
+}
+
+// Оптимизированный компонент ввода
+const Input = memo(({ onChange }) => {
+  const renderCount = useRef(0);
+  renderCount.current++;
+  
+  return (
+    <div className="input-container">
+      <input 
+        type="text" 
+        className="input-field" 
+        name="value" 
+        onChange={onChange} 
+      />
+      <RenderCounter count={renderCount.current} />
+    </div>
+  );
+});
+
+// Оптимизированный корневой компонент формы
+const Root = memo(() => {
+  const [value, setValue] = React.useState('');
+  const renderCount = useRef(0);
+  renderCount.current++;
+  
+  // Стабилизированный обработчик изменений
+  const handleChange = useCallback((event) => {
+    setValue(event.target.value);
+  }, []);
+  
+  // Мемоизированное значение для отображения
+  const displayedValue = useMemo(() => `Введенное значение: ${value}`, [value]);
+
+  return (
+    <form className="form-container">
+      {displayedValue}
+      <RenderCounter count={renderCount.current} />
+      <Input onChange={handleChange} />
+    </form>
+  );
+});
+
+// Главный компонент
+export default function TaskTwo() {
+  const update = useUpdate();
+  const renderCount = useRef(0);
+  renderCount.current++;
+  
+  return (
+    <div className="TaskTwo">
+      <button onClick={update}>Обновить компонент</button>
+      <RenderCounter count={renderCount.current} />
+      <Root />
+    </div>
+  );
 }
